762737ac46a8af4c0151cb0c790a045e72367d50
ChristinaSi <wir13dvx@studserv.uni-leipzig.de>
2019-03-11 14:31:27 +0100

    Remove unnecessary tag names.

--- src/main/java/org/jqassistant/contrib/plugin/c/impl/scanner/TagNameConstants.java
+++ src/main/java/org/jqassistant/contrib/plugin/c/impl/scanner/TagNameConstants.java
@@ -8,18 +8,10 @@ package org.jqassistant.contrib.plugin.c.impl.scanner;
 public class TagNameConstants {
 	public static final String TRANSLATIONUNIT = "de.fosd.typechef.parser.c.TranslationUnit";
 	public static final String ENTRY = "entry";
-	public static final String SPECIFIERS = "specifiers";
-	public static final String DECLARATOR = "declarator";
-	public static final String ID = "id";
 	public static final String NAME = "name";
-	public static final String STATEMENT = "stmt";
 	public static final String INNERSTATEMENTS = "innerStatements";
 	public static final String VALUE = "value";
-	public static final String DECLARATION = "decl";
-	public static final String DECLARATIONSPECIFICATION = "declSpecs";
-	public static final String INIT = "init";
 	public static final String CONDITION = "de.fosd.typechef.conditional.Opt";
 	public static final String FEATUREEXPRESSION = "fexpr";
 	public static final String ISUNION = "isUnion";
-	public static final String ENUMERATORS = "enumerators";
 }
